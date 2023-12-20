e01330b480484dbb4e2e80b9940c13c30af4a548
jpokan <jorgeou1993@gmail.com>
2023-12-20 13:47:56 -0500

    fix: refactor to webgl plugin

--- app.vue
+++ app.vue
@@ -1,3 +1,3 @@
 <template>
-	<Experience />
+	<div></div>
 </template>
---
+++ plugins/webgl.client.js
@@ -0,0 +1,5 @@
+import Experience from "~/assets/webgl/experience.js"
+
+export default defineNuxtPlugin(nuxtApp => {
+    new Experience()
+})
