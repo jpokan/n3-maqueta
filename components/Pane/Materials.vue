9f78137a48f8417f69652879492768061e49f538
jpokan <jorgeou1993@gmail.com>
2024-01-05 05:31:25 -0500

    fix: select materials

--- components/Pane/Materials.vue
+++ components/Pane/Materials.vue
@@ -3,7 +3,7 @@
 		<UButton @click="createMaterial" icon="i-heroicons-plus" size="xs" variant="solid" color="white"
 			label="New Material" />
 		<div v-for="(material, index) in materials" @click="select(material)"
-			:class="{ 'bg-gray-200 dark:bg-gray-950': selected }"
+			:class="material.uuid === selection.material[0].uuid ? 'bg-gray-200 dark:bg-gray-950' : ''"
 			class="flex flex-row items-center px-0.5 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-md">
 			<PaneInput :value="material.name" @update:value="update(material, $event)" />
 			<UButton @click="removeMaterial(index, material)" icon="i-heroicons-trash" variant="link" color="gray" size="xs"
@@ -17,14 +17,12 @@ import * as THREE from "three";
 import { materials } from "assets/webgl/materials";
 import { selection } from "assets/webgl/helpers";
 
+onMounted(() => {
+	selection.material.push(materials[0])
+})
+
 // TO DO: get all unique materials from src_scene.children
 function parseMaterials() { }
-
-const selected = computed(() => {
-	if (selection.material.length > 0) {
-		return selection.material[0].uuid ? true : false;
-	}
-});
 
 function select(material) {
 	selection.material = [];
