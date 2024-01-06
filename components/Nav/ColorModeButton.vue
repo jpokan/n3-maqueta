eda85f09ff539a320ac669f1c376e8ed4339bff9
jpokan <jorgeou1993@gmail.com>
2024-01-06 04:22:27 -0500

    fix: remove fallback

--- components/Nav/ColorModeButton.vue
+++ components/Nav/ColorModeButton.vue
@@ -2,10 +2,10 @@
 	<ClientOnly>
 		<UButton :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'" size="xs" color="white"
 			variant="solid" aria-label="Theme" @click="isDark = !isDark" />
-		<template #fallback>
-			<UButton icon="i-heroicons-computer-desktop-16-solid" size="xs" color="white" variant="solid"
-				aria-label="Theme" />
-		</template>
+		<!-- <template #fallback> -->
+		<!-- 	<UButton icon="i-heroicons-computer-desktop-16-solid" size="xs" color="white" variant="solid" -->
+		<!-- 		aria-label="Theme" /> -->
+		<!-- </template> -->
 	</ClientOnly>
 </template>
 
